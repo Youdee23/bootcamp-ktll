b4778efed383cd031d72f9a79e8db63a7e803e74
Uduak <uetimfon@gmail.com>
2024-07-23 20:49:02 +0100

    project_airbnb

--- projects/models/base_model.py
+++ projects/models/base_model.py
@@ -1,47 +1,47 @@
 import uuid
-import datetime
+from datetime import datetime
 
 
-class BaseModel():
+class BaseModel:
     """ A class called BaseModel"""
-    def __init__(self, id, created_at, updated_at):
+    def __init__(self, *args, **kwargs):
         """
         Instantiating the class with new attributes
         id: The unique id for each BaseModel; it will be converted to a string
         created_at: The current date and time the instance is created
         updated_at: The current date and time an instance is created and 
-        it will be updated everytime an object is changed.  
+        it will be updated everytime an object is changed.
+        *args: It takes a variable number of positional arguments
+        **kwargs: It takes a variable number of keyword arguments  
         """
-        self.id = id
-        self.created_at = created_at
-        self.updated_at = updated_at
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        self.args = args
+        self.kwargs = kwargs
 
-    def __str__(self, id):
-        return f' BaseModel {self.id} {self.__dict__}'
+        if kwargs:
+            kwargs = {}
+            for k, v in vars(self).items():
+                kwargs[k] = v
+
+    def __str__(self):
+        return f'[{self.__class__.__name__}] ({self.id}) <{self.__dict__}>'
     
-    def save(self, date_time):
+    def save(self):
         """ Updates the attribute updated_at with the current date_time"""
+        self.updated_at = datetime.now()  # to get the current date/time
         
-
     def to_dict(self):
         """ Returns a dictionary of all key/values of __dict__ of the instance"""
-        instance_dict = self.__dict__
-        instance_dict['__class__'] = 'BaseModel'
+        instance_dict = self.__dict__.copy()
+        instance_dict[__class__] = self.__class__.__name__
+        instance_dict['created_at'] = instance_dict['created_at'].isoformat()
+        instance_dict['updated_at'] = instance_dict['updated_at'].isoformat()
         return instance_dict
 
 
-    # def get_id(self, unique_id):
-    #     """ Retrieves and returns a string of the new unique id for each BaseModel """
-    #     unique_id = uuid.uuid4()
-    #     return str(unique_id)
-    
-    # def get_created_at(self, date_time):
-    #     """ Returns the current date and time an instance is created"""
-    #     return date_time
-    
-    # def get_updated_at(self, date_time):
-    #     """ Returns the updated date_time each time an object is changed"""
-    #     return date_time
+
     
 
 
