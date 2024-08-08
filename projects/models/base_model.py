1eb08a3692d350abe8140a5b7c612ae9934d6416
Uduak <uetimfon@gmail.com>
2024-08-08 22:56:09 +0100

    base model

--- projects/models/base_model.py
+++ projects/models/base_model.py
@@ -20,8 +20,8 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
-        if not self.__class__.__name__.__dict__:
-            print(storage.new()) 
+        # if not self.__class__.__name__.__dict__:
+        #     print(storage.new()) 
 
         if kwargs:
             kwargs = {}
@@ -30,6 +30,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
+            storage.new()
 
     def __str__(self):
         return f'[{self.__class__.__name__}] ({self.id}) <{self.__dict__}>'
@@ -37,6 +38,7 @@ class BaseModel:
     def save(self):
         """ Updates the attribute updated_at with the current date_time"""
         self.updated_at = datetime.now()  # to get the current date/time
+        storage.save()
         
     def to_dict(self):
         """ Returns a dictionary of all key/values of __dict__ of the instance"""
@@ -46,7 +48,7 @@ class BaseModel:
         instance_dict['updated_at'] = instance_dict['updated_at'].isoformat()
         return instance_dict
     
-    print(storage.save())
+    
     
 
 
