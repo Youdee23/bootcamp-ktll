710f246451eda9c65c83c7ef8db02b47abd9ccd9
Uduak <uetimfon@gmail.com>
2024-09-06 03:37:15 +0100

    class state

---
+++ projects/models/state.py
@@ -0,0 +1,9 @@
+from projects.models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    A class called State that inherits from BaseModel
+    """
+    def __init__(self):
+        self.name = ""
