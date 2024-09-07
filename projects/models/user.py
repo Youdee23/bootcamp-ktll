c5659982bfb58dcef0715e92fb43493d56acf6d9
Uduak <uetimfon@gmail.com>
2024-09-07 11:52:52 +0100

    class user

--- projects/models/user.py
+++ projects/models/user.py
@@ -5,10 +5,8 @@ class User(BaseModel):
     """
     A class called User that inherits from BaseModel
     """
-    def __init__(self):
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
-
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
 
