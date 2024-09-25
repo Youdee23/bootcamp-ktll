eb468d860b6ddbb71c5a7f4c008760e4bff46cee
Uduak <uetimfon@gmail.com>
2024-09-25 18:37:53 +0100

    test for ShoppingCart

--- test_project/test_shopping_cart.py
+++ test_project/test_shopping_cart.py
@@ -1,15 +1,18 @@
 import pytest
 from shopping_cart import ShoppingCart
 
+
 def test_can_add_item_to_cart():
     cart = ShoppingCart(5)
     cart.add("wig")
     assert cart.size() == 1
 
+
 def test_when_item_added_then_cart_contains_item():
     cart = ShoppingCart(5)
     cart.add("wig")
     assert "wig" in cart.get_items()
+
 
 def test_when_add_more_then_max_size():
     cart = ShoppingCart(5)
@@ -18,5 +21,8 @@ def test_when_add_more_then_max_size():
             cart.add("apple4")
         cart.add("apple")
 
+
 def test_can_get_total_price():
-    pass
+    cart = ShoppingCart(5)
+    cart.get_total_price()
+    assert cart.get_total_price() == 0
