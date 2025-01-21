1943e6375630e5299e142356e5c0c10fa5c3b284
Uduak <uetimfon@gmail.com>
2025-01-21 19:43:56 +0100

    test for ShoppingCart

--- test_project/test_shopping_cart.py
+++ test_project/test_shopping_cart.py
@@ -5,19 +5,23 @@ from shopping_cart import ShoppingCart
 def cart():
     return ShoppingCart(5)
 
+
 def test_can_add_item_to_cart(cart):
     cart.add("wig")
     assert cart.size() == 1
 
+
 def test_when_item_added_then_cart_contains_item(cart):
     cart.add("wig")
     assert "wig" in cart.get_items()
+
 
 def test_when_add_more_then_max_size(cart):
     with pytest.raises(OverflowError): # creates a context
         for _ in range(5): # 1,2,3,4,5,6
             cart.add("apple4")
         cart.add("apple")
+
 
 def test_can_get_total_price(cart):
     cart.add("milk")
@@ -29,3 +33,11 @@ def test_can_get_total_price(cart):
     }
 
     assert cart.get_total_price(price_map) == 3900
+
+
+def test_can_remove_item_from_cart(cart):
+    cart.add("milk")
+    cart.add("coffee")
+    assert cart.size() == 2
+    cart.remove("milk")
+    assert cart.size() == 1
